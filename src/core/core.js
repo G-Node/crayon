731a4993604522329f4896e6c2ef3d9f23eaec69
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2012-01-15 16:21:16 +0530

    added deepcopy
    Signed-off-by: Dipanjan Mukherjee <dipanjan.mu@gmail.com>

--- src/core/core.js
+++ src/core/core.js
@@ -1,3 +1,11 @@
+var deepcopy = function(o) {
+  // Creates new objects. Not passing references. :)
+  var F = function() {};
+  F.prototype = o;
+  return new F();
+};
+
 window.crayon = {
-  version: '0.0.1'
+  version    : '0.0.1',
+  deepcopy   : deepcopy
 };
