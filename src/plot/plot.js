feea85e771e49c5c10093f477cccf8dd550de6e2
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2012-01-16 21:10:38 +0530

    sensible domain, range changes handled
    Signed-off-by: Dipanjan Mukherjee <dipanjan.mu@gmail.com>

--- src/plot/plot.js
+++ src/plot/plot.js
@@ -1,8 +1,10 @@
 var plot = function (data, name) {
-  var context = this;
+  var context    = this,
+      domainFlag = false,
+      rangeFlag  = false;
 
-  var xmax = ymax = Number.MIN_VALUE; 
-  var xmin = ymin = Number.MAX_VALUE;
+  var xmax = ymax = Number.MIN_VALUE,
+      xmin = ymin = Number.MAX_VALUE;
 
   for ( var t in data ) {
     // compute the domain and range of the signal passed
@@ -12,16 +14,43 @@ var plot = function (data, name) {
     ymax = (ymax < data[t]) ? data[t] : ymax;
     ymin = (ymin > data[t]) ? data[t] : ymin;
   }
+  if ( !context.grid ) {
+    // This is the first time, right
+    context.xmax = xmax;
+    context.xmin = xmin;
+    context.ymax = ymax;
+    context.ymin = ymin;
 
-  context.xmax = xmax;
-  context.xmin = xmin;
-  context.ymax = ymax;
-  context.ymin = ymin;
+    rangeFlag = domainFlag = true;
+  } else {
+
+    if ( context.xmax < xmax ) { 
+      domainFlag = true;
+      context.xmax = xmax;
+    }
+    if ( context.xmin > xmin ) { 
+      domainFlag = true;
+      context.xmin = xmin;
+    }
+    if ( context.ymax < ymax ) { 
+      rangeFlag = true;
+      context.ymax = ymax;
+    }
+    if ( context.ymin > ymin ) { 
+      domainFlag = true;
+      context.ymin = ymin;
+    }
+
+  }
 
   if (!context.grid) { crayon.bus.publish('FirstPlot', [context]) }
 
-  crayon.bus.publish('DomainChanged', [context]);
-  crayon.bus.publish('RangeChanged', [context]);
+  if ( domainFlag ) {
+    crayon.bus.publish('DomainChanged', [context]);
+  }  
+  if ( rangeFlag ) {
+    crayon.bus.publish('RangeChanged', [context]);
+  }
 }
 
 crayon.handle.plot = plot;
