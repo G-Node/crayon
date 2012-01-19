73feb213d08af232b572371293b5177046d556e9
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2012-01-19 22:17:50 +0530

    add updateRange and updateDomain functions
    Signed-off-by: Dipanjan Mukherjee <dipanjan.mu@gmail.com>

--- src/core/init.js
+++ src/core/init.js
@@ -8,6 +8,68 @@ crayon.bus = {
               }
 }
 
+var updateRange = function(range) {
+  // pass a [ymin, ymax] array to this function to update the current
+  // context's range.  If a range update is done, publish the
+  // RangeChanged event and return true. Else, return false. 
+  var context = this,
+      flag    = false;
+
+  if (context.ymax == undefined) {
+    // first time
+    flag         = true;
+    context.ymin = range[0];
+    context.ymax = range[1];
+  } else {
+    if ( context.ymin > range[0] ) { 
+      flag = true;
+      context.ymin = range[0];
+    }if ( context.ymax < range[1] ) { 
+      flag = true;
+      context.ymax = range[1];
+    }
+  }
+
+  if ( flag ) {
+    context.y = d3.scale.linear().domain([context.ymin, context.ymax])
+                               .range([0, context.h - context.p ]);
+    crayon.bus.publish('RangeChanged', [context]);
+  }
+
+  return flag;
+}
+
+var updateDomain = function(domain) {
+  // pass a [xmin, xmax] array to this function to update the current
+  // context's domain.  If a domain update is done, publish the
+  // DomainChanged event and return true. Else, return false. 
+  var context = this,
+      flag    = false;
+
+  if (context.xmax == undefined) {
+    // first time
+    flag         = true;
+    context.xmin = domain[0];
+    context.xmax = domain[1];
+  } else {
+    if ( context.xmin > domain[0] ) { 
+      flag = true;
+      context.xmin = domain[0];
+    }if ( context.xmax < domain[1] ) { 
+      flag = true;
+      context.xmax = domain[1];
+    }
+  }
+  
+  if ( flag ) {
+   context.x = d3.scale.linear().domain([context.xmin, context.xmax])
+                               .range([0, context.w - context.p ]);
+   crayon.bus.publish('DomainChanged', [context]);
+  }
+
+  return flag;
+}
+
 // This sets up the initialization functions which should work like :
 //  <script>
 //    var c = crayon.init('<<css_div_selector_here>>');
@@ -39,6 +101,9 @@ var init = function (selector) {
 
   handle.ymax = undefined;
   handle.ymin = undefined;
+
+  handle.updateDomain = updateDomain;
+  handle.updateRange = updateRange;
 
   handle.element = element;
 
--- src/plot/drawsignal.js
+++ src/plot/drawsignal.js
@@ -27,46 +27,15 @@ var drawSignal = function (data, name) {
     ymax = (ymax < u.y) ? u.y : ymax;
     ymin = (ymin > u.y) ? u.y : ymin;
   }
-  if ( context.xmax == undefined ) {
-    // This is the first time, right
-    context.xmax = xmax;
-    context.xmin = xmin;
-    context.ymax = ymax;
-    context.ymin = ymin;
-
-    rangeFlag = domainFlag = true;
-  } else {
-    if ( context.xmax < xmax ) { 
-      domainFlag = true;
-      context.xmax = xmax;
-    }
-    if ( context.xmin > xmin ) { 
-      domainFlag = true;
-      context.xmin = xmin;
-    }
-    if ( context.ymax < ymax ) { 
-      rangeFlag = true;
-      context.ymax = ymax;
-    }
-    if ( context.ymin > ymin ) { 
-      domainFlag = true;
-      context.ymin = ymin;
-    }
-  }
+  
+  context.updateDomain([xmin, xmax]);
+  context.updateRange([ymin, ymax]);
 
   // create the x and y coordinate mappings
   context.x = d3.scale.linear().domain([context.xmin, context.xmax])
                                .range([0, context.w - context.p ]);
   context.y = d3.scale.linear().domain([context.ymin, context.ymax])
                                .range([0, context.h - context.p ]);
-
-
-  if ( domainFlag ) {
-    crayon.bus.publish('DomainChanged', [context]);
-  }  
-  if ( rangeFlag ) {
-    crayon.bus.publish('RangeChanged', [context]);
-  }
 
   crayon.bus.publish('SignalAdded', [context, data, name]);
 }
