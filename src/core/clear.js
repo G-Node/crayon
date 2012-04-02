5eec09ed65d0d37a2affe6c0562cda6311a74919
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2012-04-02 22:17:05 +0530

    clear() fix: add signals as a container after removing it
    Signed-off-by: Dipanjan Mukherjee <dipanjan.mu@gmail.com>

--- src/core/clear.js
+++ src/core/clear.js
@@ -1,4 +1,12 @@
 crayon.handle.clear = function () {
   /* Just remove all the signals. */
   this.signals.remove();
+
+  this.signals = this.element
+    .append('g')
+    .attr('id', 'signals')
+    .attr('transform', 'translate('+ this.p.toString() + ',' + 
+                                    (parseInt(this.h) - this.p).toString() + ')' +
+                       'scale(1,-1)'
+    );
 };
