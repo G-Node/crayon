916867d76747b72d9d9e80071b303637b509c3f3
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2012-04-02 22:27:13 +0530

    also removing grid in clear();
    Signed-off-by: Dipanjan Mukherjee <dipanjan.mu@gmail.com>

--- src/core/clear.js
+++ src/core/clear.js
@@ -1,6 +1,18 @@
 crayon.handle.clear = function () {
-  /* Just remove all the signals. */
+  /* Just remove all the signals. and the grid */
   this.signals.remove();
+  this.grid.remove();
+
+
+  /* Then add the bare minimums */
+  this.grid = this.element 
+    .append('g')
+    .attr('id', 'grid')
+    .attr('transform', 'translate('+ this.p.toString() + ',' + 
+                                    (parseInt(this.h) - this.p).toString() + ')' +
+                       'scale(1,-1)'
+    );
+
 
   this.signals = this.element
     .append('g')
@@ -9,4 +21,11 @@ crayon.handle.clear = function () {
                                     (parseInt(this.h) - this.p).toString() + ')' +
                        'scale(1,-1)'
     );
+
+  this.color = crayon.color_init();
+
+  this.xmax = undefined;
+  this.ymax = undefined;
+  this.xmin = undefined;
+  this.ymin = undefined;
 };
