cb7568afcdeb4838314bbdfac05c73ebbd2a3e24
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2013-05-28 11:27:44 +0200

    Include Function.inherit() in main js file

--- src/inheritance.js
+++ src/inheritance.js
@@ -1,13 +1,5 @@
-// ---------- file: inheritance.js ---------- //
 
 
-/**
- * Mixin on Function in order to provide easier inheritance.
- * Adds a property parent to the prototype of the function which
- * contains the parent object or function.
- *
- * @param parent The super class or object.
- */
 Function.prototype.inherits = function(parent) {
   if (typeof(parent.constructor) === 'function') {
     this.prototype = new parent();
